9472419429dfa746d6d0d868565a1dd3c857c8c3
Mayeenul Islam <wz.islam@gmail.com>
2021-04-10 00:04:36 +0600

    Copy/Design: Indicate copy status
    Closes #3

--- assets/js/utilities.js
+++ assets/js/utilities.js
@@ -1,14 +1,28 @@
-function copyText(elementId) {
-    /* Get the text field */
-    var copyText = document.getElementById(elementId);
+function copyText(target, button) {
+    var target = document.getElementById(target);
+    var copyIcon = button.querySelector(".copy-icon");
+    var copyText = button.querySelector(".copy-text");
 
     /* Select the text field */
-    copyText.select();
-    copyText.setSelectionRange(0, 99999); /* For mobile devices */
+    target.select();
+    target.setSelectionRange(0, 99999); /* For mobile devices */
 
     /* Copy the text inside the text field */
     document.execCommand("copy");
 
-    //TODO: enable indicative icon
-    // alert("Copied the text: " + copyText.value);
+    /* Handle the UI indications on copied the texts */
+    copyIcon.classList.remove("bi-clipboard");
+    copyIcon.classList.add("bi-check-circle-fill", "text-success");
+
+    copyText.innerText = 'কপি করা হয়েছে';
+    copyText.classList.remove("visually-hidden");
+
+    /* Set the copy icon and button to its original state */
+    setTimeout(function () {
+        copyIcon.classList.remove("bi-check-circle-fill", "text-success");
+        copyIcon.classList.add("bi-clipboard");
+
+        copyText.innerText = "কপি করুন";
+        copyText.classList.add("visually-hidden");
+    }, 1200);
 }
